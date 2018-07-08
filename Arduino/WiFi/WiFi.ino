4a647193a7ccab28e4315d9c4803e5bf352edad3
Guilherme Aramizo Ribeiro <garamizo@mtu.edu>
2018-07-08 19:04:44 -0400

    Add code to scan networks

--- Arduino/WiFi/WiFi.ino
+++ Arduino/WiFi/WiFi.ino
@@ -1,6 +1,6 @@
 #include<SPI.h>
 #include<WiFi.h>
-//#define LEDPin 22
+#define LEDPin 22
 //#define buttonPin 0
 
 // Network information
@@ -10,6 +10,7 @@ const char password[] = "";
 
 void setup() {
   // put your setup code here, to run once:
+    pinMode(LEDPin , OUTPUT);
     Serial.begin(9600); //Initialize serial port
     while(!Serial) {
       ;
@@ -32,11 +33,48 @@ void loop() {
    
 
     delay(10000);
-    Serial.print("You r connected");
+    //Serial.print("You r connected");
     Serial.println(WiFi.status());// Show the user a connection is successful
-  }
-  
 
+    //Scan wifi networks available
+    Serial.print("Scanning wifi networks");
+    int numSsid = WiFi.scanNetworks();
+
+    if (numSsid == -1) {
+       Serial.println("Couldn't get a wifi connection");
+       
+    while (true);
+  
+  }
+ Serial.println("number of available networks:");
+ Serial.println(numSsid); 
+
+ // print the network names found:
+
+ for(int x = 0; x < numSsid; x++) {
+    Serial.println(WiFi.SSID(x));
+    Serial.print("\tSignal: ");
+  
+ }
+ledControl();//To Control Ledpin of WiFi module
+delay(10000);
+ 
+  }
+
+  void ledControl() {
+
+     if(Serial.available() > 0)  
+   { 
+    int state = Serial.read();
+    Serial.print("State :");
+    Serial.println(state);
+
+    if (state =='0')
+       digitalWrite(LEDPin, LOW);
+    else if(state == '1')
+       digitalWrite(LEDPin, HIGH); 
+  }
+  }
 
 
   
